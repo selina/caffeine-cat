2351a11a9b690baee2cc40a12b29c71a8d6fe9ad
Selina <enigmacat@users.noreply.github.com>
2014-07-14 17:32:27 -0400

    got generateCup to work; now can generate static blocks at random x-location at the top

--- selinawang.spritebuilder/Source/Gameplay.m
+++ selinawang.spritebuilder/Source/Gameplay.m
@@ -6,31 +6,56 @@
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
+
 #import "Gameplay.h"
+#import "Cup.h"
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
     CCNode *_contentNode;
     NSMutableArray *_coffeeCupTypeArray;
     NSMutableArray *_coffeeCupPositionArray;
+    
 }
 
 
-_coffeeCupTypeArray = [NSMutableArray arrayWithObjects: @"orange", @"yellow", @"red", nil];
 
 
 
 - (void)didLoadFromCCB {
     // tell the scene to accept touches
     self.userInteractionEnabled = TRUE;
+    
+    _coffeeCupTypeArray = [NSMutableArray arrayWithObjects: @"orange", @"yellow", @"red", nil];
+    [self generateCup];
 }
 
 -(void)generateCup{
-    int randomint = arc4random() % 4;
-    [CCBReader load:[_coffeeCupTypeArray objectAtIndex:randomint]];
+    //generate random cup type (empty, half full, or full)
+    
+    int randomint = arc4random() % 3;
+    Cup *cupinstance = (Cup*)[CCBReader load:[_coffeeCupTypeArray objectAtIndex:randomint]];
+    
+    srandom(time(NULL));
+    
+    CGFloat x_position = CCRANDOM_0_1() * _contentNode.contentSize.width;
+    float y_position = _contentNode.contentSize.height;
+    CGPoint cupLocation = ccp(x_position, y_position);
+    
+    cupinstance.positionType = CCPositionTypeNormalized;
+    cupinstance.position = cupLocation;
+    
+    [self addChild:cupinstance];
 }
 
-
+-(void)update:(CCTime)delta {
+    //check for how much time has passed to see if it's time to generate new cup
+    
+    
+    
+    //move cups down by increment 
+    
+}
 - (void)removeCup {
     [self removeFromParent];
 }
